b6bd27a00101749f5fa9837992f1fba4eecc1a55
juharuo <juharuo@projru.fastroi.fi>
2016-04-10 10:38:12 +0300

    The uuid library updated to pborman's. A tester-script added.

--- dry-run.sh
+++ dry-run.sh
@@ -3,5 +3,9 @@ if test -z "$(which jq)";then
     echo "jq not found (https://stedolan.github.io/jq)"
     exit 1
 fi
+if test -z "$(redis-cli ping)";then
+    echo "NOTE: Redis is not running (no PONG received), this will not succeed."
+    exit 1
+fi
 TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d '{"Username":"haku","Password":"testing"}' localhost:5000/token-auth|jq -r ".token")
 curl -H "Authorization: Bearer $TOKEN" localhost:5000/test/hello
